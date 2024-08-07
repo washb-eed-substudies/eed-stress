9daef0a1a33c9f5bba62eb3d7cc3f379a2cd3d7b
Lexie Bialek <lexiebialek@lexies-mbp-2.myfiosgateway.com>
2024-08-07 16:48:09 -0400

    Update enrollment characteristics table 1 for EED-Stress manuscript

---
+++ IQR.R
@@ -0,0 +1,8 @@
+# child age
+median((d$ageday_st2/30.4167), na.rm = TRUE)
+quantile((d$ageday_st2/30.4167), 0.25, na.rm = TRUE)
+quantile((d$ageday_st2/30.4167), 0.75, na.rm = TRUE)
+
+median((d$ageday_st3/30.4167), na.rm = TRUE)
+quantile((d$ageday_st3/30.4167), 0.25, na.rm = TRUE)
+quantile((d$ageday_st3/30.4167), 0.75, na.rm = TRUE)
